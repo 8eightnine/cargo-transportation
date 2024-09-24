31c7a7fea94cf1d89447590942137f5b9095bf29
Anton <Tonerxdd@gmail.com>
2024-09-24 23:28:48 +0700

    First commit

---
+++ Hash/Hash.cs
@@ -0,0 +1,23 @@
+﻿using System.Security.Cryptography;
+using System.Text;
+
+namespace cargo_transportation.Hash
+{
+    internal static class Hash
+    {
+        public static string hashPassword(string password)
+        {
+            MD5 md5 = MD5.Create();
+
+            byte[] b = Encoding.ASCII.GetBytes(password);
+            byte[] hash = md5.ComputeHash(b);
+
+            StringBuilder sb = new StringBuilder();
+            foreach (var a in hash)
+                sb.Append(a.ToString("X2"));
+
+            return sb.ToString();
+        }
+
+    }
+}
